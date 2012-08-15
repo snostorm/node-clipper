e993ff04ee1fa0d9759367ebc7d3e6ead58412aa
Sean Ouimet <seano@redmantech.ca>
2012-08-15 16:33:18 -0600

    Re-adds check for arguments as array, returns results wrapped in array (to account for the future possibility of MultiArray input.

--- src/init.cc
+++ src/init.cc
@@ -12,44 +12,59 @@ using namespace ClipperLib;
 v8::Handle<Value> Method(const Arguments& args) {
   v8::HandleScope scope;
 
-  // if (args[0]->IsArray()) {
-    v8::Local<v8::Array> points = v8::Array::Cast(*args[0]);
+  if (args[0]->IsArray()) {
+    try {
+      v8::Local<v8::Array> points = v8::Array::Cast(*args[0]);
 
-    Polygons polyshape(1);
-    Polygon polyout;
+      ClipperLib::Polygons polyshape(1), polydone;
 
-    // Populate the subject polygon
-    for (int i = 0, limiti = points->Length(); i < limiti; i += 2) {
-      // pair = v8::Array::New(2);
-      // pair->set(0, points[0]); 
-      v8::Local<v8::Value> pairA = points->Get(i);
-      v8::Local<v8::Value> pairB = points->Get(i+1);
-      polyshape[0].push_back(
-        IntPoint(
-          pairA->NumberValue(),
-          pairB->NumberValue()
-        )
-      );
-    }
-
-    ClipperLib::OffsetPolygons(polyshape, polyshape, args[1]->NumberValue(), jtMiter, 3);
-
-    if (polyshape.size() > 0) {
-      Handle<Array> outPoints = Array::New(); //polyshape[0].size() * 2
-      v8::Local<v8::Number> total = v8::Number::New(polyshape[0].size());
-
-      for (Polygon::size_type k = 0; k < polyshape[0].size(); ++k) {
-        IntPoint ip = polyshape[0][k];
-        v8::Local<v8::Number> x = v8::Number::New(ip.X);
-        v8::Local<v8::Number> y = v8::Number::New(ip.Y);
-        outPoints->Set(outPoints->Length(), x);
-        outPoints->Set(outPoints->Length(), y);
+      // Populate the subject polygon
+      for (int i = 0, limiti = points->Length(); i < limiti; i += 2) {
+        // pair = v8::Array::New(2);
+        // pair->set(0, points[0]); 
+        v8::Local<v8::Value> pairA = points->Get(i);
+        v8::Local<v8::Value> pairB = points->Get(i+1);
+        polyshape[0].push_back(
+          IntPoint(
+            pairA->NumberValue(),
+            pairB->NumberValue()
+          )
+        );
       }
 
-      return scope.Close(outPoints); 
-    }
+      bool wasReversed = false;
+      if (!ClipperLib::Orientation(polyshape[0])) {
+        ClipperLib::ReversePolygons(polyshape[0]);
+        wasReversed = true;
+      }
 
-  // }
+      double miterLimit = 3.0;
+
+      ClipperLib::OffsetPolygons(polyshape, polydone, args[1]->NumberValue(), jtMiter, miterLimit);
+
+      if (!wasReversed && ClipperLib::Orientation(polydone[0])) {
+        ClipperLib::ReversePolygons(polydone);
+      }
+
+      if (polydone.size() > 0) {
+        Handle<Array> outPolygons = Array::New();
+        for (Polygon::size_type g = 0; g < polydone.size(); ++g) {
+          Handle<Array> outPoints = Array::New();
+          for (Polygon::size_type k = 0; k < polydone[g].size(); ++k) {
+            IntPoint ip = polydone[g][k];
+            v8::Local<v8::Number> x = v8::Number::New(ip.X);
+            v8::Local<v8::Number> y = v8::Number::New(ip.Y);
+            outPoints->Set(outPoints->Length(), x);
+            outPoints->Set(outPoints->Length(), y);
+          }
+          outPolygons->Set(g, outPoints);
+        }
+        return scope.Close(outPolygons); 
+      }
+    } catch (...) {
+      return scope.Close(v8::Boolean::New(false));
+    }
+  }
   return scope.Close(v8::Undefined());
 }
 
