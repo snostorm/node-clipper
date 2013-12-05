dba9bef5250e9cca4236ca6ff367d3ec1b3c39d8
runlevel3@goedel <git@runlevel3.de>
2013-12-05 15:58:44 +0100

    updated example for new datastructure

--- examples/offset.js
+++ examples/offset.js
@@ -5,14 +5,14 @@ clipper = require('../build/Release/clipper')
   * arguments: flattened_coordinate_path, offset
   * where: offset can be positive or negative (negative will go inside the polygon, positive will expand outside)
 */
-flattened_coordinate_path = [0, 0, 0, 10, 10, 10, 10, 0]
+flattened_coordinate_path = [[[0, 0], [0, 10], [10, 10], [10, 0]]]
 
 // A negative offset gets an offset inside our Polygon:
-inner_highlight_path = clipper.offset(flattened_coordinate_path, -2)
+inner_highlight_path = clipper.offset(flattened_coordinate_path, 'integer', -2)
 console.log(inner_highlight_path)
 // returns [ [ 2, 2, 8, 2, 8, 8, 2, 8 ] ]
 
 // A positive offset gets an offset outside our Polygon:
-outter_highlight_path = clipper.offset(flattened_coordinate_path, 2)
+outter_highlight_path = clipper.offset(flattened_coordinate_path, 'integer', 2)
 console.log(outter_highlight_path)
 // returns [ [ -2, 12, -2, -2, 12, -2, 12, 12 ] ]
